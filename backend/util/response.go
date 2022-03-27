7b67c018e9f8f3219073e7e2cd5e2783c5c492e2
John Exodus Mercado <jexodusmercado@gmail.com>
2022-03-27 22:57:44 +0800

    added response utility

--- backend/util/response.go
+++ backend/util/response.go
@@ -1,6 +1,10 @@
 package util
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/go-playground/validator/v10"
+    "errors"
+)
 
 // Response struct
 type Response struct {
@@ -8,9 +12,38 @@ type Response struct {
     Message string      `json:"message"`
     Data    interface{} `json:"data"`
 }
+type ErrorMsg struct {
+    Field string `json:"field"`
+    Message   string `json:"message"`
+}
 
 // ErrorJSON : json error response function
-func ErrorJSON(c *gin.Context, statusCode int, data interface{}) {
+func ErrorJSON(c *gin.Context, statusCode int, err error) {
+    var ve validator.ValidationErrors
+        if errors.As(err, &ve) {
+            out := make([]ErrorMsg, len(ve))
+            for i, fe := range ve {
+                out[i] = ErrorMsg{fe.Field(), getErrorMsg(fe)}
+            }
+        c.JSON(statusCode, gin.H{"errors": out})
+    }
+}
+
+func getErrorMsg(fe validator.FieldError) string {
+    switch fe.Tag() {
+        case "required":
+            return "This field is required"
+        case "lte":
+            return "Should be less than " + fe.Param()
+        case "gte":
+            return "Should be greater than " + fe.Param()
+        case "email":
+            return "Must be in email format"
+    }
+    return "Unknown error"
+}
+
+func CustomErrorJson(c *gin.Context, statusCode int, data interface{}) {
     c.JSON(statusCode, gin.H{"error": data})
 }
 
