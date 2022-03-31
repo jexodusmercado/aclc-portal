1f49d1658fdd19aa81bb89458dfecc091849413f
John Exodus Mercado <jexodusmercado@gmail.com>
2022-04-01 00:12:09 +0800

    removed msg wrapper in api

--- backend/util/response.go
+++ backend/util/response.go
@@ -49,5 +49,5 @@ func CustomErrorJson(c *gin.Context, statusCode int, data interface{}) {
 
 // SuccessJSON : json error response function
 func SuccessJSON(c *gin.Context, statusCode int, data interface{}) {
-    c.JSON(statusCode, gin.H{"msg": data})
+    c.JSON(statusCode, data)
 }
