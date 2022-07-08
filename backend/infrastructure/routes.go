e3c29ea1af38cae39cb037671c65b8e7a5f157e0
John Exodus Mercado <jexodusmercado@gmail.com>
2022-07-08 17:56:49 +0800

    update

--- backend/infrastructure/routes.go
+++ backend/infrastructure/routes.go
@@ -10,36 +10,36 @@ import (
 
 //GinRouter -> Gin Router
 type GinRouter struct {
-    Gin *gin.Engine
+	Gin *gin.Engine
 }
 
 //NewGinRouter all the routes are defined here
 func NewGinRouter() GinRouter {
 
-    httpRouter := gin.Default()
+	httpRouter := gin.Default()
 
-    config := cors.DefaultConfig()
-    // config.AllowAllOrigins = true
-    config.AllowOrigins = []string{"http://localhost:3001","http://localhost:3001/","http://localhost:3000"}
-    // config.AllowCredentials = true
-    config.AddAllowMethods("OPTIONS")
-    config.AllowHeaders = []string{
-        "Origin",
-        "Content-Length",
-        "Content-Type",
-        "Authorization",
-    }
+	config := cors.DefaultConfig()
+	// config.AllowAllOrigins = true
+	config.AllowOrigins = []string{"http://localhost:3001", "http://localhost:3001/", "http://localhost:3000", "http://http://aclc-bucket.s3-website-ap-southeast-1.amazonaws.com/"}
+	// config.AllowCredentials = true
+	config.AddAllowMethods("OPTIONS")
+	config.AllowHeaders = []string{
+		"Origin",
+		"Content-Length",
+		"Content-Type",
+		"Authorization",
+	}
 
-    httpRouter.Use(static.Serve("/public", static.LocalFile("./public", true)))
+	httpRouter.Use(static.Serve("/public", static.LocalFile("./public", true)))
 
-    httpRouter.GET("/", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"data": "Up and Running..."})
-    })
+	httpRouter.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "Up and Running..."})
+	})
 
-    httpRouter.Use(cors.New(config))
+	httpRouter.Use(cors.New(config))
 
-    return GinRouter{
-        Gin: httpRouter,
-    }
+	return GinRouter{
+		Gin: httpRouter,
+	}
 
 }
